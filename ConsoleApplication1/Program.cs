fac1707488812ceb40902061ac42dd02d2dba922
vb <vb@VB-PC>
2016-12-13 23:23:31 +0200

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Decimal C  ;
+           C = 4M;
+
+            Console.Write(C + 3);
+            Console.ReadKey(); 
+        }
+    }
+}
